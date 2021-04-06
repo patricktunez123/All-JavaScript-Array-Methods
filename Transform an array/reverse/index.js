a5896a44041dfc67b7e598dd0f34105cc74a3b63
Patrick Tunezerwane <tunezepatrick@gmail.com>
2021-04-06 13:10:08 +0200

    reverse

---
+++ Transform an array/reverse/index.js
@@ -0,0 +1,12 @@
+// The method arr.reverse reverses the order of elements in arr.
+// For instance:
+let arr = [1, 2, 3, 4, 5];
+arr.reverse();
+
+alert( arr ); // 5,4,3,2,1
+
+
+//example two
+let arr2 = [1, 2, 50, 90, 40]
+const rev = arr2.reverse()
+console.log(rev)
