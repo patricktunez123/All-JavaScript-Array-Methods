33126a660be41e3b1380a373d705aef48740090b
Patrick Tunezerwane <tunezepatrick@gmail.com>
2021-04-06 13:28:26 +0200

    concat

---
+++ Add and remove items/concat/index.js
@@ -0,0 +1,41 @@
+//The method arr.concat creates a new array that includes values from other arrays and additional items.
+
+//The syntax is:
+
+//arr.concat(arg1, arg2...)
+
+let arr = [1, 2];
+
+// create an array from: arr and [3,4]
+alert( arr.concat([3, 4]) ); // 1,2,3,4
+
+// create an array from: arr and [3,4] and [5,6]
+alert( arr.concat([3, 4], [5, 6]) ); // 1,2,3,4,5,6
+
+// create an array from: arr and [3,4], then add values 5 and 6
+alert( arr.concat([3, 4], 5, 6) ); // 1,2,3,4,5,6
+
+//Normally, it only copies elements from arrays. Other objects, even if they look like arrays, are added as a whole:
+
+let arr = [1, 2];
+
+let arrayLike = {
+  0: "something",
+  length: 1
+};
+
+alert( arr.concat(arrayLike) ); // 1,2,[object Object]
+
+// …But if an array-like object has a special Symbol.isConcatSpreadable property, then it’s treated as an array by concat: its elements are added instead:
+
+let arr = [1, 2];
+
+let arrayLike = {
+  0: "something",
+  1: "else",
+  [Symbol.isConcatSpreadable]: true,
+  length: 2
+};
+
+alert( arr.concat(arrayLike) ); // 1,2,something,else
+
